5e40c46d9c2fd094d20c740b52e3e5b957c6138c
Jesus Arias <gabrielj82@gmail.com>
2022-08-17 00:11:45 -0300

    boton copy js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 var texto = document.getElementById("txtEncryptDecrypt");
-var textoIn = texto.value.toLowerCase();
 var rectangle1 = document.querySelector("#rectangle1");
 var rectangle2 = document.querySelector("#rectangle2");
 var txtEncrypt = document.querySelector("#txtEncryptDecrypt");
-var btnEncrypt= document.querySelector(".btnEncrypt");
-var btnDecrypt= document.querySelector(".btnDecrypt");
-var btnCopy= document.querySelector(".btnCopy");
+var btnEncrypt = document.querySelector(".btnEncrypt");
+var btnDecrypt = document.querySelector(".btnDecrypt");
+var btnCopy = document.querySelector(".btnCopy");
 
 //funcion ocultar rectangulo 1 y mostrar el rectangulo 2
 function rectangulo() {
@@ -15,43 +14,48 @@ function rectangulo() {
 
 //funcion encriptar
 function encriptarTexto() {
-    var nuevoTexto = textoIn.replace(/e/igm, 'enter');
-    var nuevoTexto = nuevoTexto.replace(/i/igm, 'imes');
-    var nuevoTexto = nuevoTexto.replace(/a/igm, 'ai');
-    var nuevoTexto = nuevoTexto.replace(/o/igm, 'ober');
-    var nuevoTexto = nuevoTexto.replace(/u/igm, 'ufat');
+    let textoIn = texto.value.toString().toLowerCase();
+    let nuevoTexto = textoIn
+        .replace(/e/igm, 'enter')
+        .replace(/i/igm, 'imes')
+        .replace(/a/igm, 'ai')
+        .replace(/o/igm, 'ober')
+        .replace(/u/igm, 'ufat');
     document.getElementById("areaEncrypt").innerHTML = nuevoTexto;
 }
 
 //funcion desencriptar
 function desencriptarTexto() {
-    var nuevoTexto = textoIn.replace(/enter/igm, 'e');
-    var nuevoTexto = nuevoTexto.replace(/imes/igm, 'i');
-    var nuevoTexto = nuevoTexto.replace(/ai/igm, 'a');
-    var nuevoTexto = nuevoTexto.replace(/ober/igm, 'o');
-    var nuevoTexto = nuevoTexto.replace(/ufat/igm, 'u');
+    let textoIn = texto.value;
+    let nuevoTexto = textoIn
+        .replace(/enter/igm, 'e')
+        .replace(/imes/igm, 'i')
+        .replace(/ai/igm, 'a')
+        .replace(/ober/igm, 'o')
+        .replace(/ufat/igm, 'u');
     document.getElementById("areaEncrypt").innerHTML = nuevoTexto;
 }
 
 //Funcion Copiar
 function copy() {
-    let copyText = document.querySelector("#areaEncrypt");
-    copyText.select();
-document.getElementById("txtEncryptDecrypt").value = "";
-  }
-
+    var content = document.getElementById('areaEncrypt');
+    content.select();
+    document.execCommand('copy');
+    document.getElementById("txtEncryptDecrypt").value = "";
+}
 
 //Botones
 btnEncrypt.addEventListener('click', function () {
     rectangulo();
     encriptarTexto();
- });
 
- btnDecrypt.addEventListener('click', function () {
+});
+
+btnDecrypt.addEventListener('click', function () {
     rectangulo();
     desencriptarTexto();
- });
+});
 
- btnCopy.addEventListener('click', function () {
-      copy();
- });
+btnCopy.addEventListener('click', function () {
+    copy();
+});
